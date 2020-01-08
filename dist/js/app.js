75ac6385edef016dbd438368df96edd7a12aa13d
unknown <trujillorodriguezjavier@gmail.com>
2020-01-08 17:24:08 +0100

    sliding container

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,6 @@
 const burguerBtn = document.querySelector(".burguer");
 const mobileNav = document.querySelector(".mobile-navbar");
+const allCont = document.querySelector(".all-container");
 
 burguerBtn.addEventListener("click", toggleMenu);
 
@@ -10,10 +11,12 @@ function toggleMenu() {
   if (!showMenu) {
     mobileNav.classList.add("active");
     burguerBtn.classList.add("close");
+    allCont.classList.add("shift");
     showMenu = true;
   } else {
     mobileNav.classList.remove("active");
     burguerBtn.classList.remove("close");
+    allCont.classList.remove("shift");
     showMenu = false;
   }
 }
