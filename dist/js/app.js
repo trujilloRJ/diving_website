a291d1a5ee67e6a82bbf1e42b044abca0c67d6e4
unknown <trujillorodriguezjavier@gmail.com>
2020-01-08 18:34:51 +0100

    scroll navbar and team link

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,13 @@
+// Variables from the DOM
 const burguerBtn = document.querySelector(".burguer");
 const mobileNav = document.querySelector(".mobile-navbar");
 const allCont = document.querySelector(".all-container");
+const navBar = document.querySelector(".navbar");
+const showcase = document.querySelector(".showcase");
 
+// burguer menu interaction
 burguerBtn.addEventListener("click", toggleMenu);
-
-// Set Initial State of Menu
 let showMenu = false;
-
 function toggleMenu() {
   if (!showMenu) {
     mobileNav.classList.add("active");
@@ -20,3 +21,22 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// scroll nav interaction
+const options = {
+  rootMargin: "-100px 0px 0px 0px"
+};
+const showcaseObserver = new IntersectionObserver(function(
+  entries,
+  showcaseObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      navBar.classList.add("scrolled");
+    } else {
+      navBar.classList.remove("scrolled");
+    }
+  });
+},
+options);
+showcaseObserver.observe(showcase);
